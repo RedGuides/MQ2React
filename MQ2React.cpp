f197a3455fec17238f7bd47f1460d67e6be4e668
Dr. Whom <drwhom@outlook.com>
2020-05-01 20:24:13 -0400

    Added additional comments explaining the TLOs.

--- MQ2React.cpp
+++ MQ2React.cpp
@@ -12,14 +12,108 @@ PLUGIN_VERSION(0.1);
 PreSetup("MQ2React");
 
 // Function Declarations
-VOID ReactCommand(PSPAWNINFO pChar, PCHAR szLine);
 BOOL TLOReact(PCHAR szIndex, MQ2TYPEVAR& Ret);
 VOID LoadConfig(const char* configname);
-int GetReactIdx(const std::string& nickname);
 void PrintHelp();
 
 // Global Declarations
 Yaml::Node root;
+
+/*
+@brief Determines if a react exists based on its nickname.
+@param nickname Name of the react to look for.
+@return -1 if the item wasn't found, otherwise its index in the list.
+*/
+int GetReactIdx(const std::string& nickname) {
+	PCHARINFO pCharInfo = GetCharInfo();
+	Yaml::Node& Reacts = root[pCharInfo->Name]["reacts"];
+
+	int react_idx = 0;
+	for (auto itR = Reacts.Begin(); itR != Reacts.End(); itR++) {
+		Yaml::Node& react = (*itR).second;
+
+		if (react["nickname"].As<std::string>() == nickname) {
+			return react_idx;
+		}
+		react_idx++;
+	}
+	// Default return
+	return -1;
+}
+
+void PrintHelp()
+{
+	WriteChatf("MQ2React");
+	WriteChatf("Ineract with MQ2React via /react <verb> <optarg> <optarg> <optarg>");
+	WriteChatf("Reaction configuration is per character name per character name in mq2react.ini.");
+	WriteChatf("help - display this message.");
+	WriteChatf("add <nickname> <condition> <action> - add a new reaction to your config with <condition> and <action> labeled <nickname>. Disabled by default.");
+	WriteChatf("remove <nickname> - remove a reaction from your config file that has the label <nickname> .");
+	WriteChatf("enable <nickname> - enable a reaction with label <nickname>.");
+	WriteChatf("disable <nickname> - disable a reaction with label <nickname>.");
+}
+
+// Parse /react <verb> <opt:nickname> <opt:condition> <opt:action>
+// /help - print command help
+VOID ReactCommand(PSPAWNINFO pChar, PCHAR szLine)
+{
+	char Verb[MAX_STRING] = { 0 };
+	char Nickname[MAX_STRING] = { 0 };
+	char Condition[MAX_STRING] = { 0 };
+	char Action[MAX_STRING] = { 0 };
+	PCHARINFO pCharInfo = GetCharInfo();
+
+	GetArg(Verb, szLine, 1);
+
+	if (!_stricmp(Verb, "help")) {
+		PrintHelp();
+	}
+	if (!_stricmp(Verb, "add")) {
+		GetArg(Nickname, szLine, 2);
+		if (!strlen(Nickname)) PrintHelp();
+
+		GetArg(Condition, szLine, 3);
+		if (!strlen(Condition)) PrintHelp();
+
+		GetArg(Action, szLine, 4);
+		if (!strlen(Action)) PrintHelp();
+
+		if (GetReactIdx(Nickname) == -1) {
+			Yaml::Node& NewReact = root[pCharInfo->Name]["reacts"].PushFront();
+			NewReact["nickname"] = Nickname;
+			NewReact["condition"] = Condition;
+			NewReact["action"] = Action;
+			NewReact["enabled"] = "0";
+		}
+	}
+	if (!_stricmp(Verb, "remove")) {
+		GetArg(Nickname, szLine, 2);
+		if (!strlen(Nickname)) PrintHelp();
+
+		int react_idx = GetReactIdx(Nickname);
+		if (react_idx != -1) {
+			root[pCharInfo->Name]["reacts"].Erase(react_idx);
+		}
+	}
+	if (!_stricmp(Verb, "enable")) {
+		GetArg(Nickname, szLine, 2);
+		if (!strlen(Nickname)) PrintHelp();
+
+		int react_idx = GetReactIdx(Nickname);
+		if (react_idx != -1) {
+			root[pCharInfo->Name]["reacts"][react_idx]["enabled"] = "1";
+		}
+	}
+	if (!_stricmp(Verb, "disable")) {
+		GetArg(Nickname, szLine, 2);
+		if (!strlen(Nickname)) PrintHelp();
+
+		int react_idx = GetReactIdx(Nickname);
+		if (react_idx != -1) {
+			root[pCharInfo->Name]["reacts"][react_idx]["enabled"] = "0";
+		}
+	}
+}
 
 // Called once, when the plugin is to initialize
 PLUGIN_API VOID InitializePlugin(VOID)
@@ -63,69 +157,6 @@ PLUGIN_API VOID OnPulse(VOID)
 	//DebugSpewAlways("MQ2React::OnPulse()");
 }
 
-// Parse /react <verb> <opt:nickname> <opt:condition> <opt:action>
-// /help - print command help
-
-VOID ReactCommand(PSPAWNINFO pChar, PCHAR szLine)
-{
-	char Verb[MAX_STRING] = { 0 };
-	char Nickname[MAX_STRING] = { 0 };
-	char Condition[MAX_STRING] = { 0 };
-	char Action[MAX_STRING] = { 0 };
-	PCHARINFO pCharInfo = GetCharInfo();
-
-	GetArg(Verb, szLine, 1);
-
-	if (!_stricmp(Verb, "help")) {
-		PrintHelp();
-	}
-	if (!_stricmp(Verb, "add")) {
-		GetArg(Nickname, szLine, 2);
-		if (!strlen(Nickname)) PrintHelp();
-
-		GetArg(Condition, szLine, 3);
-		if (!strlen(Condition)) PrintHelp();
-
-		GetArg(Action, szLine, 4);
-		if (!strlen(Action)) PrintHelp();
-
-		if (GetReactIdx(Nickname) == -1) {
-			Yaml::Node& NewReact = root[pCharInfo->Name]["reacts"].PushFront();
-			NewReact["nickname"] = Nickname;
-			NewReact["condition"] = Condition;
-			NewReact["action"] = Action;
-			NewReact["enabled"] = "0";
-		}	
-	}
-	if (!_stricmp(Verb, "remove")) {
-		GetArg(Nickname, szLine, 2);
-		if (!strlen(Nickname)) PrintHelp();
-
-		int react_idx = GetReactIdx(Nickname);
-		if (react_idx != -1) {
-			root[pCharInfo->Name]["reacts"].Erase(react_idx);
-		}
-	}
-	if (!_stricmp(Verb, "enable")) {
-		GetArg(Nickname, szLine, 2);
-		if (!strlen(Nickname)) PrintHelp();
-
-		int react_idx = GetReactIdx(Nickname);
-		if (react_idx != -1) {
-			root[pCharInfo->Name]["reacts"][react_idx]["enabled"] = "1";
-		}
-	}
-	if (!_stricmp(Verb, "disable")) {
-		GetArg(Nickname, szLine, 2);
-		if (!strlen(Nickname)) PrintHelp();
-
-		int react_idx = GetReactIdx(Nickname);
-		if (react_idx != -1) {
-			root[pCharInfo->Name]["reacts"][react_idx]["enabled"] = "0";
-		}
-	}
-}
-
 void LoadConfig(const char* configname)
 {
 	Yaml::Parse(root, configname);
@@ -158,40 +189,6 @@ void LoadConfig(const char* configname)
 	}
 }
 
-void PrintHelp()
-{
-	WriteChatf("MQ2React");
-	WriteChatf("Ineract with MQ2React via /react <verb> <optarg> <optarg> <optarg>");
-	WriteChatf("Reaction configuration is per character name per character name in mq2react.ini.");
-	WriteChatf("help - display this message.");
-	WriteChatf("add <nickname> <condition> <action> - add a new reaction to your config with <condition> and <action> labeled <nickname>. Disabled by default.");
-	WriteChatf("remove <nickname> - remove a reaction from your config file that has the label <nickname> .");
-	WriteChatf("enable <nickname> - enable a reaction with label <nickname>.");
-	WriteChatf("disable <nickname> - disable a reaction with label <nickname>.");
-}
-
-/*
-@brief Determines if a react exists based on its nickname.
-@param nickname Name of the react to look for.
-@return -1 if the item wasn't found, otherwise its index in the list.
-*/
-int GetReactIdx(const std::string& nickname) {
-	PCHARINFO pCharInfo = GetCharInfo();
-	Yaml::Node& Reacts = root[pCharInfo->Name]["reacts"];
-
-	int react_idx = 0;
-	for (auto itR = Reacts.Begin(); itR != Reacts.End(); itR++) {
-		Yaml::Node& react = (*itR).second;
-
-		if (react["nickname"].As<std::string>() == nickname) {
-			return react_idx;
-		}
-		react_idx++;
-	}
-	// Default return
-	return -1;
-}
-
 class MQ2ReactType : public MQ2Type {
 public:
 	enum Members {
@@ -208,6 +205,11 @@ public:
 	}
 	~MQ2ReactType() {}
 
+	/*
+	* React.Action[nickname] -- Returns the action associated with the react 'nickname'
+	* React.Condition[nickname] -- Returns the condition associated with the react 'nickname'
+	* React.Enabled[nickname] -- Returns if the react 'nickname' is enabled or not
+	*/
 	bool GetMember(MQ2VARPTR VarPtr, PCHAR Member, PCHAR Index, MQ2TYPEVAR& Dest)
 	{
 		PMQ2TYPEMEMBER pMember = MQ2ReactType::FindMember(Member);
