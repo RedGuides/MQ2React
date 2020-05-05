eb243e5d2a4a474ce9765b1272c3ebbd0b8a7aa5
Dr. Whom <drwhom@outlook.com>
2020-05-04 21:42:01 -0400

    Fixed some iterator naming in getreactidx. Added a PrintReacts function to print reacts. Added the /react list command. Fixed the infinite loop that somehow happened when push_front was called from the yaml library when adding new reacts. Moved functions around to remove the need to define the prototypes.

--- MQ2React.cpp
+++ MQ2React.cpp
@@ -11,11 +11,6 @@
 PLUGIN_VERSION(0.1);
 PreSetup("MQ2React");
 
-// Function Declarations
-BOOL TLOReact(PCHAR szIndex, MQ2TYPEVAR& Ret);
-VOID LoadConfig(const char* configname);
-void PrintHelp();
-
 // Global Declarations
 static Yaml::Node root;
 
@@ -29,8 +24,8 @@ int GetReactIdx(const std::string& nickname) {
 	Yaml::Node& Reacts = root[pCharInfo->Name]["reacts"];
 
 	int react_idx = 0;
-	for (auto itR = Reacts.Begin(); itR != Reacts.End(); itR++) {
-		Yaml::Node& react = (*itR).second;
+	for (auto itr = Reacts.Begin(); itr != Reacts.End(); itr++) {
+		Yaml::Node& react = (*itr).second;
 
 		if (react["nickname"].As<std::string>() == nickname) {
 			return react_idx;
@@ -41,16 +36,28 @@ int GetReactIdx(const std::string& nickname) {
 	return -1;
 }
 
+
+void PrintReacts() {
+	PCHARINFO pCharInfo = GetCharInfo();
+	Yaml::Node& Reacts = root[pCharInfo->Name]["reacts"];
+
+	for (auto itr = Reacts.Begin(); itr != Reacts.End(); itr++) {
+		Yaml::Node& react = (*itr).second;
+		WriteChatfSafe("%s",react["nickname"].As<std::string>().c_str());
+	}
+}
+
 void PrintHelp()
 {
 	WriteChatf("MQ2React");
 	WriteChatf("Ineract with MQ2React via /react <verb> <optarg> <optarg> <optarg>");
 	WriteChatf("Reaction configuration is per character name per character name in mq2react.ini.");
-	WriteChatf("help - display this message.");
-	WriteChatf("add <nickname> <condition> <action> - add a new reaction to your config with <condition> and <action> labeled <nickname>. Disabled by default.");
-	WriteChatf("remove <nickname> - remove a reaction from your config file that has the label <nickname> .");
-	WriteChatf("enable <nickname> - enable a reaction with label <nickname>.");
-	WriteChatf("disable <nickname> - disable a reaction with label <nickname>.");
+	WriteChatf("/react help - display this message.");
+	WriteChatf("/noparse /react add <nickname> <condition> <action> - add a new reaction to your config with <condition> and <action> labeled <nickname>. Disabled by default.");
+	WriteChatf("/react remove <nickname> - remove a reaction from your config file that has the label <nickname> .");
+	WriteChatf("/react enable <nickname> - enable a reaction with label <nickname>.");
+	WriteChatf("/react disable <nickname> - disable a reaction with label <nickname>.");
+	WriteChatf("/react list - Display all condition nicknames.");
 }
 
 // Parse /react <verb> <opt:nickname> <opt:condition> <opt:action>
@@ -79,12 +86,13 @@ VOID ReactCommand(PSPAWNINFO pChar, PCHAR szLine)
 		if (!strlen(Action)) PrintHelp();
 
 		if (GetReactIdx(Nickname) == -1) {
-			Yaml::Node& NewReact = root[pCharInfo->Name]["reacts"].PushFront();
+			Yaml::Node& NewReact = root[pCharInfo->Name]["reacts"].PushBack();
 			NewReact["nickname"] = Nickname;
 			NewReact["condition"] = Condition;
 			NewReact["action"] = Action;
 			NewReact["enabled"] = "0";
 		}
+		Yaml::Serialize(root, INIFileName);
 	}
 	if (!_stricmp(Verb, "remove")) {
 		GetArg(Nickname, szLine, 2);
@@ -113,85 +121,11 @@ VOID ReactCommand(PSPAWNINFO pChar, PCHAR szLine)
 			root[pCharInfo->Name]["reacts"][react_idx]["enabled"] = "0";
 		}
 	}
-}
-
-// Called once, when the plugin is to initialize
-PLUGIN_API VOID InitializePlugin(VOID)
-{
-	DebugSpewAlways("Initializing MQ2React");
-	AddCommand("/react", ReactCommand);
-	AddMQ2Data("React", TLOReact);
-}
-
-// Called once, when the plugin is to shutdown
-PLUGIN_API VOID ShutdownPlugin(VOID)
-{
-	DebugSpewAlways("Shutting down MQ2React");
-	RemoveCommand("/react");
-
-	//Remove commands, MQ2Data items, hooks, etc.
-	//RemoveMQ2Benchmark(bmMyBenchmark);
-	//RemoveCommand("/mycommand");
-	//RemoveXMLFile("MQUI_MyXMLFile.xml");
-}
-
-// Called after entering a new zone
-PLUGIN_API VOID OnZoned(VOID)
-{
-	DebugSpewAlways("MQ2React::OnZoned()");
-}
-
-// Called once directly after initialization, and then every time the gamestate changes
-PLUGIN_API VOID SetGameState(DWORD GameState)
-{
-	DebugSpewAlways("MQ2React::SetGameState()");
-	if (GameState == GAMESTATE_INGAME) {
-		LoadConfig(INIFileName);
+	if (!_stricmp(Verb, "list")) {
+		PrintReacts();
 	}
 }
 
-// This is called every time MQ pulses
-PLUGIN_API VOID OnPulse(VOID)
-{
-	// DONT leave in this debugspew, even if you leave in all the others
-	//DebugSpewAlways("MQ2React::OnPulse()");
-}
-
-void LoadConfig(const char* configname)
-{
-	PCHARINFO pCharInfo = GetCharInfo();
-	if (!pCharInfo) return;
-
-	if (!_FileExists(configname)) {
-		Yaml::Serialize(root, configname);
-	}
-
-	// Must call after the file check or you could error out.
-	Yaml::Parse(root, configname);
-
-	// Try and load the current character's reacts, if they aren't in the config yet, then add
-	// them with a blank list of reacts.
-	Yaml::Node& char_config = root[pCharInfo->Name];
-
-	// If the character doesn't exist we'll create a configuration file entry for them and provide
-	// a disabled default react entry in case they want to edit the config file instead of use ingame commands.
-	if (char_config.IsNone()) {
-		if (EQADDR_SERVERNAME[0]) {
-			root[pCharInfo->Name]["Server"] = std::string(EQADDR_SERVERNAME);
-		}
-		else {
-			root[pCharInfo->Name]["Server"] = "Unknown";
-		}
-		Yaml::Node& DefaultReact = root[pCharInfo->Name]["reacts"].PushFront();
-		DefaultReact["nickname"] = "DefaultExample";
-		DefaultReact["condition"] = "${Me.PctHPs} == 100 && ${Me.CombatState.NotEqual[COMBAT]}";
-		DefaultReact["action"] = "/echo If you enable this it will spam you with echos whenyou're at full health out of combat.";
-		DefaultReact["enabled"] = "0";
-		Yaml::Serialize(root, configname);
-	}
-}
-
-class MQ2ReactType* pReactType = nullptr;
 class MQ2ReactType : public MQ2Type {
 public:
 	enum Members {
@@ -270,6 +204,7 @@ public:
 private:
 	CHAR _buf[MAX_STRING];
 };
+class MQ2ReactType* pReactType = nullptr;
 
 BOOL TLOReact(char* szIndex, MQ2TYPEVAR& Dest)
 {
@@ -277,3 +212,80 @@ BOOL TLOReact(char* szIndex, MQ2TYPEVAR& Dest)
 	Dest.Type = pReactType;
 	return true;
 }
+
+void LoadConfig(const char* configname)
+{
+	PCHARINFO pCharInfo = GetCharInfo();
+	if (!pCharInfo) return;
+
+	if (!_FileExists(configname)) {
+		Yaml::Serialize(root, configname);
+	}
+
+	// Must call after the file check or you could error out.
+	Yaml::Parse(root, configname);
+
+	// Try and load the current character's reacts, if they aren't in the config yet, then add
+	// them with a blank list of reacts.
+	Yaml::Node& char_config = root[pCharInfo->Name];
+
+	// If the character doesn't exist we'll create a configuration file entry for them and provide
+	// a disabled default react entry in case they want to edit the config file instead of use ingame commands.
+	if (char_config.IsNone()) {
+		if (EQADDR_SERVERNAME[0]) {
+			root[pCharInfo->Name]["Server"] = std::string(EQADDR_SERVERNAME);
+		}
+		else {
+			root[pCharInfo->Name]["Server"] = "Unknown";
+		}
+		Yaml::Node& DefaultReact = root[pCharInfo->Name]["reacts"].PushFront();
+		DefaultReact["nickname"] = "DefaultExample";
+		DefaultReact["condition"] = "${Me.PctHPs} == 100 && ${Me.CombatState.NotEqual[COMBAT]}";
+		DefaultReact["action"] = "/echo If you enable this it will spam you with echos when you're at full health out of combat.";
+		DefaultReact["enabled"] = "0";
+		Yaml::Serialize(root, configname);
+	}
+}
+
+// Called once, when the plugin is to initialize
+PLUGIN_API VOID InitializePlugin(VOID)
+{
+	DebugSpewAlways("Initializing MQ2React");
+	AddCommand("/react", ReactCommand);
+	AddMQ2Data("React", TLOReact);
+}
+
+// Called once, when the plugin is to shutdown
+PLUGIN_API VOID ShutdownPlugin(VOID)
+{
+	DebugSpewAlways("Shutting down MQ2React");
+	RemoveCommand("/react");
+	RemoveMQ2Data("React");
+	//Remove commands, MQ2Data items, hooks, etc.
+	//RemoveMQ2Benchmark(bmMyBenchmark);
+	//RemoveCommand("/mycommand");
+	//RemoveXMLFile("MQUI_MyXMLFile.xml");
+}
+
+// Called after entering a new zone
+PLUGIN_API VOID OnZoned(VOID)
+{
+	DebugSpewAlways("MQ2React::OnZoned()");
+}
+
+// Called once directly after initialization, and then every time the gamestate changes
+PLUGIN_API VOID SetGameState(DWORD GameState)
+{
+	DebugSpewAlways("MQ2React::SetGameState()");
+	if (GameState == GAMESTATE_INGAME) {
+		LoadConfig(INIFileName);
+	}
+}
+
+// This is called every time MQ pulses
+PLUGIN_API VOID OnPulse(VOID)
+{
+	// DONT leave in this debugspew, even if you leave in all the others
+	//DebugSpewAlways("MQ2React::OnPulse()");
+}
+
